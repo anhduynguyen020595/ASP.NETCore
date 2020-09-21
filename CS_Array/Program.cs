ce5a00d5563d12feae668899c5f57380f89b7a99
SD - Nguyen Anh Duy <duy.nguyen1@intranet-trg.com>
2020-09-21 10:11:53 +0700

    Array in C#

---
+++ CS_Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CS_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+            Predicate<int> predicate = (int number) => {return number%2 == 0; };
+
+            int[] oddNumbers = Array.FindAll(numbers, predicate);
+
+            Action<int> printNumber = (int number)=> System.Console.WriteLine(number);
+            Array.ForEach(oddNumbers, printNumber);
+
+        }
+    }
+}
