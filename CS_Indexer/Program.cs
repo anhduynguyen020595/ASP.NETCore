ea07081a46bdd148d84be4bc73a5fd820d591950
SD - Nguyen Anh Duy <duy.nguyen1@intranet-trg.com>
2020-09-21 09:46:53 +0700

    Modified the name of folder for CS_Indexer

---
+++ CS_Indexer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndexerExam obj = new IndexerExam();
+            System.Console.WriteLine(obj[0] + " " + obj[1]);
+            obj[0] = "Tran";
+            obj[1] = "Hieu";
+            System.Console.WriteLine(obj[0] + " " + obj[1]);
+        }
+    }
+}
