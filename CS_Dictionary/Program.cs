624852b4844cebabfd321515f0fc39a2ea912b3a
SD - Nguyen Anh Duy <duy.nguyen1@intranet-trg.com>
2020-09-22 10:54:52 +0700

    Dictionary in C#

---
+++ CS_Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CS_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<string , int> count = new Dictionary<string, int>()
+           {
+                ["one"] = 1,
+                ["two"] = 2
+           };
+
+           count["three"] = 3;
+
+           var keys = count.Keys;
+           foreach(var k in keys){
+               var value = count[k];
+               System.Console.WriteLine($"{k} = {value}");
+           }
+        }
+    }
+}
