5f4574e8eca38055e5b84bf16286a10ca7fb22fd
SD - Nguyen Anh Duy <duy.nguyen1@intranet-trg.com>
2020-09-22 11:26:20 +0700

    Observable Collection in CÊ

---
+++ CS_ObservableCollection/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace CS_ObservableCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableCollection<string> obs = new ObservableCollection<string>();
+
+            obs.CollectionChanged += change;
+
+            obs.Add("ZTest1");
+            obs.Add("DTest2");
+            obs.Add("ATest3");
+            obs[2] = "AAAAA";
+
+            obs.RemoveAt(1);
+            obs.Clear();
+        }
+
+        private static void change(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (String s in e.NewItems)
+                    {
+                        System.Console.WriteLine($"Add: {s} ");
+                    }
+                    break;
+
+                case NotifyCollectionChangedAction.Reset:
+                    Console.WriteLine("Clear");
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (String s in e.OldItems)
+                        Console.WriteLine($"Remove :  {s}");
+                    break;
+
+                case NotifyCollectionChangedAction.Replace:
+                    Console.WriteLine("Repaced - " + e.NewItems[0]);
+                    break;
+            }
+        }
+    }
+}
