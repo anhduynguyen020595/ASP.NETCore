fee95ed757f0c249de71b0982547eec5f0dba84e
SD - Nguyen Anh Duy <duy.nguyen1@intranet-trg.com>
2020-09-22 10:47:28 +0700

    Linked List in C#

---
+++ CS_LinkedList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CS_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> Lessons = new LinkedList<string>();
+
+            Lessons.AddFirst("Lesson 1");
+            Lessons.AddFirst("Lesson 2");
+            Lessons.AddFirst("Lesson 3");
+            Lessons.AddFirst("Lesson 4");
+
+            System.Console.WriteLine("======== Frist to Last");
+            LinkedListNode<string> node = Lessons.First;
+            while (node != null)
+            {
+                System.Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+
+            System.Console.WriteLine("======== Last to First");
+            node = Lessons.Last;
+            while(node != null){
+                System.Console.WriteLine(node.Value);
+                node= node.Previous;
+            }
+
+        }
+    }
+}
